ef7e8fe8fbc27214c2bc43c492f87c312cd4fbaf
KyleMThornton <kylemthornton@gmail.com>
2024-01-15 12:28:29 -0800

    Update bg color to zinc-100

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SpendingCard from "./components/SpendingCard";
 
 function App() {
   return (
-    <>
+    <div className="bg-zinc-100">
       <Navbar />
       <div className="flex justify-center items-center">
         <div className="w-5/6 m-10 flex lg:flex-row flex-col">
@@ -22,7 +22,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
