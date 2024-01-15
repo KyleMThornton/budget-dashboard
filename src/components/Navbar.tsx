c88ac43691e2aadf50b7d3b8c5b81708b82fe525
KyleMThornton <kylemthornton@gmail.com>
2024-01-15 12:27:03 -0800

    Add menu button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
+import { IoMdMenu } from "react-icons/io";
+
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <IoMdMenu className="text-3xl mt-1 mr-2 cursor-pointer" />
         <span className="font-semibold text-xl tracking-tight">
           Budget Dashboard
         </span>
