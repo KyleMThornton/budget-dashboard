d5b188c54ba3cfed7377717a3beb24a5eb6d7d75
KyleMThornton <kylemthornton@gmail.com>
2024-01-19 12:46:20 -0800

    Create state and props for financial data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AccountsCard from "./components/AccountsCard";
 import BudgetCard from "./components/BudgetCard";
 import Navbar from "./components/Navbar";
@@ -5,24 +6,54 @@ import NetWorthCard from "./components/NetWorthCard";
 import SpendingCard from "./components/SpendingCard";
 
 function App() {
+  const [checkingBalance, setcheckingBalance] = useState(100);
+  const [savingsBalance, setsavingsBalance] = useState(100);
+  const [creditCard1Balance, setcreditCard1Balance] = useState(100);
+  const [creditCard2Balance, setcreditCard2Balance] = useState(100);
+  const [mortgageBalance, setmortgageBalance] = useState(100);
+  const [investmentBalance, setinvestmentBalance] = useState(100);
+  const [retirementBalance, setretirementBalance] = useState(100);
+  const [homeValue, sethomeValue] = useState(100);
+  const [personalLoanBalance, setpersonalLoanBalance] = useState(100);
+  const [incomeBudgeted, setincomeBudgeted] = useState(100);
+  const [incomeActual, setincomeActual] = useState(100);
+  const [expensesBudgeted, setexpensesBudgeted] = useState(100);
+  const [expensesActual, setexpensesActual] = useState(100);
+  const [netWorth2023, setnetWorth2023] = useState(100000);
+
   return (
-    <div>
+    <main className="bg-slate-100 flex flex-col items-center">
       <Navbar />
       <div className="flex justify-center items-center">
-        <div className="w-5/6 m-10 flex lg:flex-row flex-col">
+        <div className="m-50 flex lg:flex-row flex-col">
           <div className="m-4">
-            <BudgetCard />
+            <BudgetCard
+              incomeBudgeted={incomeBudgeted}
+              incomeActual={incomeActual}
+              expensesBudgeted={expensesBudgeted}
+              expensesActual={expensesActual}
+            />
             <SpendingCard />
           </div>
           <div className="m-4">
-            <AccountsCard />
+            <AccountsCard
+              checkingBalance={checkingBalance}
+              savingsBalance={savingsBalance}
+              creditCard1Balance={creditCard1Balance}
+              creditCard2Balance={creditCard2Balance}
+              mortgageBalance={mortgageBalance}
+              investmentBalance={investmentBalance}
+              retirementBalance={retirementBalance}
+              homeValue={homeValue}
+              personalLoanBalance={personalLoanBalance}
+            />
           </div>
           <div className="m-4">
-            <NetWorthCard />
+            <NetWorthCard netWorth2023={netWorth2023} />
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
 
