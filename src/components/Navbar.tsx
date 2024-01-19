1ade51cd9b8acc48e11e74c27aac7396565ff168
KyleMThornton <kylemthornton@gmail.com>
2024-01-19 13:49:47 -0800

    Adjust NavBar spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,9 @@
-// import { IoMdMenu } from "react-icons/io";
 import EditAccountsDrawer from "./EditAccountsDrawer";
 
 export default function Navbar() {
   return (
     <div className="w-screen bg-teal-500">
-      <nav className="w-screen flex p-5 items-center text-white justify-between container">
+      <nav className="w-screen flex p-5 items-center text-white justify-between container md:px-20">
         <EditAccountsDrawer />
         <span className="font-semibold text-xl tracking-tight">
           Budget Dashboard
