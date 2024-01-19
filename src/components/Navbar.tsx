8e3586df4d8a24ee53c9e20899273105316defb9
KyleMThornton <kylemthornton@gmail.com>
2024-01-19 12:47:37 -0800

    Implement new accounts drawer and adjust layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,13 @@
-import { IoMdMenu } from "react-icons/io";
+// import { IoMdMenu } from "react-icons/io";
+import EditAccountsDrawer from "./EditAccountsDrawer";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <IoMdMenu className="text-3xl mt-1 mr-2 cursor-pointer" />
-        <span className="font-semibold text-xl tracking-tight">
-          Budget Dashboard
-        </span>
-      </div>
+    <nav className="w-screen bg-teal-500 flex p-5 items-center text-white justify-between">
+      <EditAccountsDrawer />
+      <span className="font-semibold text-xl tracking-tight">
+        Budget Dashboard
+      </span>
     </nav>
   );
 }
