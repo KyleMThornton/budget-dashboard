1d37fb24b51463895c5c4482e9d2c2a01d5b913f
KyleMThornton <kylemthornton@gmail.com>
2024-01-26 16:39:17 -0800

    Configure state changes from drawer component

--- src/components/EditAccountsDrawer.tsx
+++ src/components/EditAccountsDrawer.tsx
@@ -13,24 +13,128 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 
-export default function EditAccountsDrawer() {
+import { useState } from "react";
+
+interface EditAccountsDrawerProps {
+  checkingBalance: number;
+  setCheckingBalance: (checkingBalance: number) => void;
+  savingsBalance: number;
+  setsavingsBalance: (savingsBalance: number) => void;
+  creditCard1Balance: number;
+  setcreditCard1Balance: (creditCard1Balance: number) => void;
+  creditCard2Balance: number;
+  setcreditCard2Balance: (creditCard2Balance: number) => void;
+  mortgageBalance: number;
+  setmortgageBalance: (mortgageBalance: number) => void;
+  investmentBalance: number;
+  setinvestmentBalance: (investmentBalance: number) => void;
+  retirementBalance: number;
+  setretirementBalance: (retirementBalance: number) => void;
+  homeValue: number;
+  sethomeValue: (homeValue: number) => void;
+  personalLoanBalance: number;
+  setpersonalLoanBalance: (personalLoanBalance: number) => void;
+  incomeBudgeted: number;
+  setincomeBudgeted: (incomeBudgeted: number) => void;
+  incomeActual: number;
+  setincomeActual: (incomeActual: number) => void;
+  expensesBudgeted: number;
+  setexpensesBudgeted: (expensesBudgeted: number) => void;
+  expensesActual: number;
+  setexpensesActual: (expensesActual: number) => void;
+}
+
+export default function EditAccountsDrawer({
+  checkingBalance,
+  setCheckingBalance,
+  savingsBalance,
+  setsavingsBalance,
+  creditCard1Balance,
+  setcreditCard1Balance,
+  creditCard2Balance,
+  setcreditCard2Balance,
+  mortgageBalance,
+  setmortgageBalance,
+  investmentBalance,
+  setinvestmentBalance,
+  retirementBalance,
+  setretirementBalance,
+  homeValue,
+  sethomeValue,
+  personalLoanBalance,
+  setpersonalLoanBalance,
+  incomeBudgeted,
+  setincomeBudgeted,
+  incomeActual,
+  setincomeActual,
+  expensesBudgeted,
+  setexpensesBudgeted,
+  expensesActual,
+  setexpensesActual,
+}: EditAccountsDrawerProps) {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  // const [checkingBalance, setcheckingBalance] = useState(100);
-  // const [savingsBalance, setsavingsBalance] = useState(100);
-  // const [creditCard1Balance, setcreditCard1Balance] = useState(100);
-  // const [creditCard2Balance, setcreditCard2Balance] = useState(100);
-  // const [mortgageBalance, setmortgageBalance] = useState(100);
-  // const [investmentBalance, setinvestmentBalance] = useState(100);
-  // const [retirementBalance, setretirementBalance] = useState(100);
-  // const [homeValue, sethomeValue] = useState(100);
-  // const [personalLoanBalance, setpersonalLoanBalance] = useState(100);
-  // const [incomeBudgeted, setincomeBudgeted] = useState(100);
-  // const [incomeActual, setincomeActual] = useState(100);
-  // const [expensesBudgeted, setexpensesBudgeted] = useState(100);
-  // const [expensesActual, setexpensesActual] = useState(100);
+
+  const [checkingInput, setCheckingInput] = useState(checkingBalance);
+  const [savingsInput, setSavingsInput] = useState(savingsBalance);
+  const [creditCard1Input, setCreditCard1Input] = useState(creditCard1Balance);
+  const [creditCard2Input, setCreditCard2Input] = useState(creditCard2Balance);
+  const [mortgageInput, setMortgageInput] = useState(mortgageBalance);
+  const [investmentInput, setInvestmentInput] = useState(investmentBalance);
+  const [retirementInput, setRetirementInput] = useState(retirementBalance);
+  const [homeInput, setHomeInput] = useState(homeValue);
+  const [personalLoanInput, setPersonalLoanInput] =
+    useState(personalLoanBalance);
+  const [incomeBudgetInput, setIncomeBudgetInput] = useState(incomeBudgeted);
+  const [incomeActualInput, setIncomeActualInput] = useState(incomeActual);
+  const [expensesBudgetInput, setExpensesBudgetInput] =
+    useState(expensesBudgeted);
+  const [expensesActualInput, setExpensesActualInput] =
+    useState(expensesActual);
+
   // const btnRef = React.useRef();
 
-  //   const handleAccountUpdates = () => {};
+  const handleButtonClick = () => {
+    if (checkingInput) {
+      setCheckingBalance(checkingInput);
+    }
+    if (savingsInput) {
+      setsavingsBalance(savingsInput);
+    }
+    if (creditCard1Input) {
+      setcreditCard1Balance(creditCard1Input);
+    }
+    if (creditCard2Input) {
+      setcreditCard2Balance(creditCard2Input);
+    }
+    if (mortgageInput) {
+      setmortgageBalance(mortgageInput);
+    }
+    if (investmentInput) {
+      setinvestmentBalance(investmentInput);
+    }
+    if (retirementInput) {
+      setretirementBalance(retirementInput);
+    }
+    if (homeInput) {
+      sethomeValue(homeInput);
+    }
+    if (personalLoanInput) {
+      setpersonalLoanBalance(personalLoanInput);
+    }
+    if (incomeBudgetInput) {
+      setincomeBudgeted(incomeBudgetInput);
+    }
+    if (incomeActualInput) {
+      setincomeActual(incomeActualInput);
+    }
+    if (expensesBudgetInput) {
+      setexpensesBudgeted(expensesBudgetInput);
+    }
+    if (expensesActualInput) {
+      setexpensesActual(expensesActualInput);
+    }
+    onClose();
+  };
 
   return (
     <>
@@ -46,31 +150,122 @@ export default function EditAccountsDrawer() {
           <DrawerBody>
             <Box>
               <FormLabel>Checking</FormLabel>
-              <Input placeholder="Checking" id="checking" />
+              <Input
+                placeholder="Checking"
+                id="checking"
+                type="number"
+                value={checkingInput}
+                onChange={(e) => setCheckingInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Savings</FormLabel>
-              <Input placeholder="Savings" id="savings" />
+              <Input
+                placeholder="Savings"
+                id="savings"
+                type="number"
+                value={savingsInput}
+                onChange={(e) => setSavingsInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Credit Card 1</FormLabel>
-              <Input placeholder="Credit Card 1" id="cc1" />
+              <Input
+                placeholder="Credit Card 1"
+                id="cc1"
+                type="number"
+                value={creditCard1Input}
+                onChange={(e) => setCreditCard1Input(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Credit Card 2</FormLabel>
-              <Input placeholder="Credit Card 2" id="cc2" />
+              <Input
+                placeholder="Credit Card 2"
+                id="cc2"
+                type="number"
+                value={creditCard2Input}
+                onChange={(e) => setCreditCard2Input(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Mortgage</FormLabel>
-              <Input placeholder="Mortgage" id="mortgage" />
+              <Input
+                placeholder="Mortgage"
+                id="mortgage"
+                type="number"
+                value={mortgageInput}
+                onChange={(e) => setMortgageInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Investment</FormLabel>
-              <Input placeholder="Investment" id="invesment" />
+              <Input
+                placeholder="Investment"
+                id="investment"
+                type="number"
+                value={investmentInput}
+                onChange={(e) => setInvestmentInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Retirement</FormLabel>
-              <Input placeholder="Retirement" id="retirement" />
+              <Input
+                placeholder="Retirement"
+                id="retirement"
+                type="number"
+                value={retirementInput}
+                onChange={(e) => setRetirementInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Home Value</FormLabel>
-              <Input placeholder="Home Value" id="home" />
+              <Input
+                placeholder="Home Value"
+                id="home"
+                type="number"
+                value={homeInput}
+                onChange={(e) => setHomeInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Personal Loan</FormLabel>
-              <Input placeholder="Personal Loan" id="loan" />
+              <Input
+                placeholder="Personal Loan"
+                id="loan"
+                type="number"
+                value={personalLoanInput}
+                onChange={(e) => setPersonalLoanInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Income - Budgeted</FormLabel>
-              <Input placeholder="Personal Loan" id="incomeBudget" />
+              <Input
+                placeholder="Income - Budgeted"
+                id="incomeBudget"
+                type="number"
+                value={incomeBudgetInput}
+                onChange={(e) => setIncomeBudgetInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Income - Actual</FormLabel>
-              <Input placeholder="Personal Loan" id="incomeActual" />
+              <Input
+                placeholder="Income - Actual"
+                id="incomeActual"
+                type="number"
+                value={incomeActualInput}
+                onChange={(e) => setIncomeActualInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Expenses - Budgeted</FormLabel>
-              <Input placeholder="Personal Loan" id="per" />
+              <Input
+                placeholder="Expenses - Budgeted"
+                id="expensesBudget"
+                type="number"
+                value={expensesBudgetInput}
+                onChange={(e) => setExpensesBudgetInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
               <FormLabel>Expenses - Actual</FormLabel>
-              <Input placeholder="Personal Loan" />
+              <Input
+                placeholder="Expenses - Actual"
+                id="expensesActual"
+                type="number"
+                value={expensesActualInput}
+                onChange={(e) => setExpensesActualInput(Number(e.target.value))}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+              />
             </Box>
           </DrawerBody>
 
@@ -78,7 +273,9 @@ export default function EditAccountsDrawer() {
             <Button variant="outline" mr={3} onClick={onClose}>
               Cancel
             </Button>
-            <Button colorScheme="blue">Save</Button>
+            <Button colorScheme="blue" onClick={handleButtonClick}>
+              Save
+            </Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
