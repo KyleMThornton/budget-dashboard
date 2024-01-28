1c1bf2462c50368d35604af2015f2315e2f2bdae
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2024-01-27 16:31:02 -0800

    Fix dark mode mismatch

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { extendTheme, ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+
+const config = {
+  initialColorMode: "system",
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode="system" />
       <App />
     </ChakraProvider>
