5f3df890e0c6f63330c1b38e878168219cc6334a
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2024-01-27 16:40:03 -0800

    Dark mode system mismatch fix

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,8 @@ const theme = extendTheme({ config });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode="system" />
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode="system" />
       <App />
     </ChakraProvider>
   </React.StrictMode>
